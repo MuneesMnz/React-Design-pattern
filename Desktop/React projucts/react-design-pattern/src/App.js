75364629c9aef622be42d709f6930e0aa168f414
muneesmnz <muneeskarakkad62@gmail.com>
2023-06-14 13:33:17 +0530

    react layout completed

---
+++ Desktop/React projucts/react-design-pattern/src/App.js
@@ -0,0 +1,22 @@
+// import { Parent } from "./Components/ListItem/Parent";
+
+import { Parent } from "./Components/MOdal/Parent";
+
+// import { Parent } from './Components/SplitScreen/Parent';
+
+function App() {
+  return (
+    //Split Screen 
+    // <Parent/>
+
+    //List Item
+    // <Parent/>
+
+    // Modal 
+    <Parent />
+
+
+  );
+}
+
+export default App;
