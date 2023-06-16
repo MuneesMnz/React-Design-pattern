3841a03a9e32bfad2ccadf3741ef43289425e96a
muneesmnz <muneeskarakkad62@gmail.com>
2023-06-16 16:08:29 +0530

    Using HighOrder Components WrappedForms And Users with axios completed

--- Desktop/React projucts/react-design-pattern/src/App.js
+++ Desktop/React projucts/react-design-pattern/src/App.js
@@ -6,11 +6,15 @@
 // import { SmallPeopleLIstItem } from "./Components/ListItem/People/SmallPeopleLIstItem";
 // import { UncontrolledModal } from "./Components/Uncontrolled/UncontrolledModal";
 import { useState } from "react";
-import { ControlledOnboardingFlow } from "./Components/Controlled/ControlledOnboardingFlow";
-import { UserInfo } from "./Components/CustomHook/UserInfo";
-import { BigSuccessButton, DangerButton } from "./Components/Functional Programming/Composition";
-import { DangerButtonPartial, SuccessButtonPartial } from "./Components/Functional Programming/partialyApply";
-import { RecursiveComponents } from "./Components/Functional Programming/RecursiveComponents";
+import { printProps } from "./Components/HighOrderComponents/printProps";
+import { UserInfoHOC } from "./Components/HighOrderComponents/UserInfo";
+import { UserInfoForm } from "./Components/HighOrderComponents/UserInfoForm";
+import { withUser } from "./Components/HighOrderComponents/withUser";
+// import { ControlledOnboardingFlow } from "./Components/Controlled/ControlledOnboardingFlow";
+// import { UserInfo } from "./Components/CustomHook/UserInfo";
+// import { BigSuccessButton, DangerButton } from "./Components/Functional Programming/Composition";
+// import { DangerButtonPartial, SuccessButtonPartial } from "./Components/Functional Programming/partialyApply";
+// import { RecursiveComponents } from "./Components/Functional Programming/RecursiveComponents";
 // import { UnControlledOnboardingFlow } from "./Components/Uncontrolled/UnControlledOnboardingFlow";
 
 // import { Parent } from "./Components/MOdal/Parent";
@@ -24,57 +28,68 @@ import { RecursiveComponents } from "./Components/Functional Programming/Recursi
 //     hobbies:['Cricket','Reading','Singing']
 //   }
 
-const StepOne=({goToNext})=>{
-return (
-  <>
-  <h1>Step 1</h1>
-  <button onClick={()=>goToNext({name:"MNZ"})} >Next</button>
-  </>
-)
-}
 
-const StepTwo=({goToNext})=>{
-  return (
-    <>
-    <h1>Step 2</h1>
-    <button onClick={()=>goToNext({age:23})} >Next</button>
-    </>
-  )
-  }
+//OnBoarding Flow
 
-const StepThree=({goToNext})=>{
-    return (
-      <>
-     <h1>Step 3</h1>
-      <button onClick={()=>goToNext({hairColor:"Black"})} >Next</button>
-      </>
-    )
-    }
+// const StepOne=({goToNext})=>{
+// return (
+//   <>
+//   <h1>Step 1</h1>
+//   <button onClick={()=>goToNext({name:"MNZ"})} >Next</button>
+//   </>
+// )
+// }
+
+// const StepTwo=({goToNext})=>{
+//   return (
+//     <>
+//     <h1>Step 2</h1>
+//     <button onClick={()=>goToNext({age:23})} >Next</button>
+//     </>
+//   )
+//   }
+
+// const StepThree=({goToNext})=>{
+//     return (
+//       <>
+//      <h1>Step 3</h1>
+//       <button onClick={()=>goToNext({hairColor:"Black"})} >Next</button>
+//       </>
+//     )
+//     }
+
+
+
+    // Functional Programs 
     
-    const nestedObject={
-      a:1,
-      b:{
-        b1:4,
-        b2:{
-          b23:"hello"
-        },
-        b3:{
-          b31:{
-            message:'Hi'
-          },
-          b32:{
-            message:'Hi'
-          }
-        }
-      },
-      c:{
-        c1:2,
-        c2:3
-      }
-    }
+    // const nestedObject={
+    //   a:1,
+    //   b:{
+    //     b1:4,
+    //     b2:{
+    //       b23:"hello"
+    //     },
+    //     b3:{
+    //       b31:{
+    //         message:'Hi'
+    //       },
+    //       b32:{
+    //         message:'Hi'
+    //       }
+    //     }
+    //   },
+    //   c:{
+    //     c1:2,
+    //     c2:3
+    //   }
+    // }
 
 
-    
+
+    // Higher Order Components 
+
+    // const UserInfoWrapped=printProps(UserInfoHOC)
+    const WithUserInfoWrapped=withUser(UserInfoHOC,2)
 
 function App() {
   // Controlled Model State And Functions
@@ -85,13 +100,16 @@ function App() {
   //   setShouldShow(false)
   // }
 
-  const [onBoardingData, setOnBoardingData] = useState({})
-  const [currentIndex, setCurrentIndex] = useState(0)
-  const onNext=stepData=>{
-   setOnBoardingData({...onBoardingData,...stepData})
-   setCurrentIndex(currentIndex + 1)
-   console.log(onBoardingData);
-}
+
+  //OnBoarding Flow
+
+//   const [onBoardingData, setOnBoardingData] = useState({})
+//   const [currentIndex, setCurrentIndex] = useState(0)
+//   const onNext=stepData=>{
+//    setOnBoardingData({...onBoardingData,...stepData})
+//    setCurrentIndex(currentIndex + 1)
+//    console.log(onBoardingData);
+// }
 //  return(
 // <>
 
@@ -158,14 +176,23 @@ function App() {
     {/* UseCurrentUser  */}
     {/* <UserInfo /> */}
 
-    <RecursiveComponents data={nestedObject} />
+    {/* Functional Programming  */}
+
+    {/* <RecursiveComponents data={nestedObject} />
     <DangerButton text="Don't Do it" />
     <BigSuccessButton text='here you go' />
     <div>
       <DangerButtonPartial text="partial Button" />
       <SuccessButtonPartial text="partial Button" />
-    </div>
+    </div> */}
 
+
+    {/* High Order Components  */}
+
+    {/* <UserInfoWrapped  a={1} b="hello" c={{name:'Mnz'}} /> */}
+
+    {/* <WithUserInfoWrapped  /> */}
+    <UserInfoForm />
 
   </div>
  )
